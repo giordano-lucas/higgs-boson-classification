a29cd6cb2f84bb619f6c405aec82efe6f422c76e
Lucas Giordano <giordano3102lucas@gmail.com>
2020-09-23 11:36:50 +0200

    add correct helper

--- helpers.py
+++ helpers.py
@@ -55,3 +55,7 @@ def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
         end_index = min((batch_num + 1) * batch_size, data_size)
         if start_index != end_index:
             yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
+
+def output_test_predictions(y):
+    
