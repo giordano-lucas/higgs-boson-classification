880cd4b492ec84194fa445859fb634c6250f1066
Lucas Giordano <giordano3102lucas@gmail.com>
2020-10-04 11:14:29 +0200

    cleaning code

--- build_polynomial.py
+++ build_polynomial.py
@@ -2,7 +2,41 @@
 """implement a polynomial basis function."""
 
 import numpy as np
+from Scaler import *
 
+# ===========================================================================
+# ===========================================================================
+# ===========================================================================
+
+class PolynomialExpansion:
+    """
+    Object that performs polynomial feature 
+    expansion and normalisation.
+    """
+    def __init__(self,degree):
+        self.scaler = None
+        self.degree = degree
+
+    def expand(self,X):
+        """
+        @param X : numpy array
+        @param degree : degree of the feature 
+        expansion
+        @return X_poly : the expanded version
+        of X
+        """
+        X_poly = build_poly(X,self.degree)    # add non interaction terms
+        if (self.scaler == None):             # in this case, a scaler is initialised
+            self.scaler = StandardScaler()    # fit the scaler
+            X_poly = self.scaler.fit(X_poly)
+        else:
+            X_poly = self.scaler.transform(X_poly) # otherwise we use the existing scaler
+        X_poly = add_bias(X_poly)             # add a bias column to X_poly
+        return X_poly
+
+# ===========================================================================
+# ===========================================================================
+# ===========================================================================
 
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
@@ -23,5 +57,22 @@ def build_poly(x, degree):
     return out
 
 def add_bias(x):
+    """
+    Add a column of ones to x 
+    """
     ones = np.ones((x.shape[0],1))
     return np.concatenate((ones,x), axis=1)
+
+
+def add_interaction(x,degree=1):
+    """
+    expand the features (columns) of a given matrix x
+    by all the interaction terms of degree = 'degree'
+    """
+    #array containing all create features plus the old ones 
+    features=[x,]
+    for i in range(x.shape[1]):
+        for j in range(x.shape[1]):
+            if i>=j: #condition to avoid duplicate (xy=yx)
+                features.append(x[:,i]*x[:,j])
+    return np.column_stack(features)
